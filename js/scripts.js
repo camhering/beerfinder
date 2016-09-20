096ae7e19a68ca0374db0166932d8bc446b79606
Anna Lovelace and Cameron Hering <anna@>
2016-09-19 20:56:14 -0700

    add the beerfinder that actually works

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+var yourBeer = function(gender, vacation) {
+  if (gender === "male" && vacation === "beach") {
+    return "Corona";
+  }
+  else if (gender === "female" && vacation === "beach") {
+    return "Pacifico";
+  }
+  else if (gender === "male" && vacation === "mountains") {
+    return "Ice Axe IPA";
+  }
+  else if (gender === "female" && vacation === "mountains") {
+    return "Basecamps Smore Stout";
+  }
+  else if (gender === "male" && vacation === "desert") {
+    return "Phream Pilsner";
+  }
+  else if (gender === "female" && vacation === "desert") {
+    return "HUB Organic Lager";
+  }
+  else if (gender === "male" && vacation === "city") {
+    return "PBR";
+  }
+  else if (gender === "female" && vacation === "city") {
+    return "Wine";
+  };
+};
+
 $(document).ready(function() {
   $("form#beer").submit(function(event) {
     //var control = $("input#control").val());
@@ -6,33 +33,8 @@ $(document).ready(function() {
     var vacation = $("select#vacation").val();
     //var motorcycle = $("input#motorcycle").val());
 
-  if (gender === "male" && vacation === "beach") {
-    var yourbeer = "Corona";
-  }
-  else if (gender === "female" && vacation === "beach") {
-    var yourbeer = "Pacifico";
-  }
-  else if (gender === "male" && vacation === "mountains") {
-    var yourbeer = "Ice Axe IPA";
-  }
-  else if (gender === "female" && vacation === "mountains") {
-    var yourbeer = "Basecamps Smore Stout";
-  }
-  else if (gender === "male" && vacation === "desert") {
-    var yourbeer = "Phream Pilsner";
-  }
-  else if (gender === "female" && vacation === "desert") {
-    var yourbeer = "HUB Organic Lager";
-  }
-  else if (gender === "male" && vacation === "city") {
-    var yourbeer = "PBR";
-  }
-  else if (gender === "female" && vacation === "city") {
-    var yourbeer = "Wine";
-  }
 
-
-
+    var yourbeer = yourBeer(gender,vacation);
     $("#yourbeer").show();
     $("#perfect").text(yourbeer);
 
